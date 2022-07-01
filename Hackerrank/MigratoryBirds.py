f98223a203a70ebd49ccbc7ae1417d5dca54162f
Ritik <varribhar@gmail.com>
2022-07-01 19:25:14 +0530

    added

---
+++ Hackerrank/MigratoryBirds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    x=list(set(arr))
+    y=sorted([(arr.count(i),i) for i in x], key=lambda x:x[0])[::-1]
+
+    for i in range(len(y)):
+        if y[0][0]==y[1][0] and y[0][1]>y[1][1]:
+            return y[1][1]
+        else:
+            return y[0][1]       
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
