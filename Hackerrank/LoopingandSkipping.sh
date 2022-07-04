0c3b8a2d05971701e5434f165206e3fa5ce1f8f5
Ritik <varribhar@gmail.com>
2022-07-04 23:25:22 +0530

    added

---
+++ Hackerrank/LoopingandSkipping.sh
@@ -0,0 +1,41 @@
+# Your task is to use for loops to display only odd natural numbers from  to .
+
+# Input Format
+
+# There is no input.
+
+# Constraints
+
+# -
+
+# Output Format
+
+# 1
+# 3
+# 5
+# .
+# .
+# .
+# .
+# .
+# 99  
+# Sample Input
+
+# -
+
+# Sample Output
+
+# 1
+# 3
+# 5
+# .
+# .
+# .
+# .
+# .
+# 99  
+
+for num in {1..99..2}
+do
+    echo $num
+done
