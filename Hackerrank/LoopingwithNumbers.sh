a60075c2825f2628fae3837c7b36d49b752e429d
Ritik <varribhar@gmail.com>
2022-07-04 23:38:55 +0530

    added

---
+++ Hackerrank/LoopingwithNumbers.sh
@@ -0,0 +1,26 @@
+
+
+# Use a for loop to display the natural numbers from  1 to 50.
+
+# Input Format
+
+# There is no input
+
+# Output Format
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# .
+# .
+# .
+# .
+# .
+# 50
+
+for num in {1..50}
+do 
+    echo $num
+done
