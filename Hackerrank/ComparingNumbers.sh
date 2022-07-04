d5e959790c454f11b01e8b08cf883dba1110744e
Ritik <varribhar@gmail.com>
2022-07-05 00:10:47 +0530

    added

--- Hackerrank/ComparingNumbers.sh
+++ Hackerrank/ComparingNumbers.sh
@@ -47,14 +47,6 @@
 # X is less than Y  
 # Explanation
 
-read X
+read c
 
-read Y
-
-if (( X > Y )); then
-    printf "X is greater than Y"
-elif (( X == Y )); then
-    printf "X is equal to Y"
-else
-    printf "X is less than Y"
-fi
+[[ "$c" == [yY] ]] && echo "YES" || echo "NO"
