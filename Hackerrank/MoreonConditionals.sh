7bb0e4c1e96ed4a5ad91f20463bf147d8ea5c8ff
Ritik <varribhar@gmail.com>
2022-07-05 00:38:29 +0530

    added

--- Hackerrank/MoreonConditionals.sh
+++ Hackerrank/MoreonConditionals.sh
@@ -36,3 +36,17 @@
 # Sample Output 2
 
 # EQUILATERAL  
+
+read a
+
+read b
+
+read c
+
+if [[ $a == $b && $b == $c ]]; then
+    echo 'EQUILATERAL';
+elif [[ $a == $b || $b == $c ]]; then
+    echo 'ISOSCELES';
+else
+    echo 'SCALENE';
+fi
