5dd69c7036bb6021e66cc7e9f776d645d126e1b8
Ritik <varribhar@gmail.com>
2022-07-05 01:03:11 +0530

    added

--- Hackerrank/cut5.sh
+++ Hackerrank/cut5.sh
@@ -1,1 +1,26 @@
-# 
+# Display a range of characters starting at the  position of a string and ending at the  position (both positions included).
+
+# Input Format
+
+# A text file containing  lines of ASCII text only.
+
+# Constraints
+
+# Output Format
+
+# The output should contain  lines.
+# Each line should contain the range of characters starting at the  position of a string and ending at the  position (both positions included).
+
+# Sample Input
+
+# Hello
+# World
+# how are you
+# Sample Output
+
+# ello
+# orld
+# ow are
+
+
+cut -c2-7
