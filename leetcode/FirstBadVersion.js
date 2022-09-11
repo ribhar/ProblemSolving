e9107a96e74e93399be79c2bbe35ee492783724d
Ritik Bhardwaj <varribhar@gmail.com>
2022-09-11 23:51:42 +0530

    leetcode added

---
+++ leetcode/FirstBadVersion.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+ var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        for(let i=n-1; i>=0; i--){
+            if(!isBadVersion(i)) return i+1
+        }
+    };
+};
