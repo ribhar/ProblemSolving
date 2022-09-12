618d2ca52c52eca654fbe2bedb33b246ba067766
Ritik Bhardwaj <varribhar@gmail.com>
2022-09-12 23:11:04 +0530

    added rotated array

--- leetcode/RotateArray.js
+++ leetcode/RotateArray.js
@@ -19,3 +19,38 @@
     }
     console.log(nums)
 }
+
+
+
+
+// second approach
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+    
+    k = k%nums.length;
+    let i=0, j=nums.length-1;
+    while(i<j){
+        [nums[i],nums[j]] = [nums[j],nums[i]]
+        i++;
+        j--;
+    }
+    i= 0;
+    j= k-1;
+    while(i<j){
+        [nums[i],nums[j]] = [nums[j],nums[i]]
+        i++;
+        j--;
+    }
+    i= k;
+    j= nums.length-1;
+    while(i<j){
+        [nums[i],nums[j]] = [nums[j],nums[i]]
+        i++;
+        j--;
+    }
+    return nums
+}
