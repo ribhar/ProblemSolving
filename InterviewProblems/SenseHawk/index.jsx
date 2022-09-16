cf5a059289d593f100518de55b134669c67cc32c
Ritik Bhardwaj <varribhar@gmail.com>
2022-09-16 22:03:06 +0530

    added

---
+++ InterviewProblems/SenseHawk/index.jsx
@@ -0,0 +1,197 @@
+<div class="relative"
+
+>
+<div >
+Hello
+</div>
+
+<div >
+Hey
+</div>
+
+</div>
+
+
+z-index
+
+BEM (Block element modifier) model
+
+
+function AddFunc(x) {
+	function add(y) {
+		return y + x;
+	};
+
+	return add;
+}
+
+var firstCall = AddFunc( 1 )
+var secondCall = AddFunc( 10 )
+
+firstCall(3); 
+secondCall(13); 
+
+function topLevel() {
+ 
+	function secondLevel(y) {
+    function inner(x){
+      console.log(x,y,z)
+		
+    }
+    return inner
+	};
+
+	return secondLevel;
+}
+ let z= 30
+topLevel()(5)(8)
+
+
+console.log(hello)
+hello='hey'
+var foo='heya'
+console.log(foo+hello)
+var hello
+
+
+const a={
+  b:{
+    c:'a'
+  }
+}
+
+const b=a
+b.b.c='b'
+console.log(a)
+console.log(b)
+
+a ={
+b:{
+    c:'a'
+}
+
+b = {
+b:{
+c:'b'
+}
+
+}
+
+}
+
+
+Techie Delight
+
+thgileD eihceT
+
+
+
+const arr = [1,2,3,4,5];
+
+function myPromise(num) {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve('Printing sequence: '+num)
+    },1000)
+  })
+   
+}
+
+√<div class="relative"
+
+>
+<div >
+Hello
+</div>
+
+<div >
+Hey
+</div>
+
+</div>
+
+
+z-index
+
+BEM (Block element modifier) model
+
+
+function AddFunc(x) {
+	function add(y) {
+		return y + x;
+	};
+
+	return add;
+}
+
+var firstCall = AddFunc( 1 )
+var secondCall = AddFunc( 10 )
+
+firstCall(3); 
+secondCall(13); 
+
+function topLevel() {
+ 
+	function secondLevel(y) {
+    function inner(x){
+      console.log(x,y,z)
+		
+    }
+    return inner
+	};
+
+	return secondLevel;
+}
+ let z= 30
+topLevel()(5)(8)
+
+
+console.log(hello)
+hello='hey'
+var foo='heya'
+console.log(foo+hello)
+var hello
+
+
+const a={
+  b:{
+    c:'a'
+  }
+}
+
+const b=a
+b.b.c='b'
+console.log(a)
+console.log(b)
+
+a ={
+b:{
+c:'a'
+}
+
+b = {
+b:{
+c:'b'
+}
+
+}
+
+}
+
+
+Techie Delight
+
+thgileD eihceT
+
+
+
+const arr = [1,2,3,4,5];
+
+function myPromise(num) {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve('Printing sequence: '+num)
+    },1000)
+  })
+   
+}
