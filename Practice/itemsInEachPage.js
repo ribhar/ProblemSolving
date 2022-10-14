e87b4c8212abdfe2dae3c6b7980e7e26370c0b1c
Ritik Bhardwaj <varribhar@gmail.com>
2022-10-14 09:50:47 +0530

    added

--- Practice/itemsInEachPage.js
+++ Practice/itemsInEachPage.js
@@ -4,12 +4,12 @@ class Splitter {
         this.itemsPerPage = itemsPerPage
     }
     numberOfPages(){
-        return this.items.length%this.itemsPerPage==0? this.items.length/this.itemsPerPage : this.items.length/this.itemsPerPage+1
+        return this.items.length%this.itemsPerPage==0? this.items.length/this.itemsPerPage : Math.floor(this.items.length/this.itemsPerPage)//+1
     }
     
 }
 let items = [1,2,"ritik","aakash",3]
-let itemsPerPage = 2
+let itemsPerPage = 6
 const pageSplitter = new Splitter(items,itemsPerPage)
 
 // console.log(pageSplitter)
