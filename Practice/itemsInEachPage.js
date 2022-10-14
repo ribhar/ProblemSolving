7c50bf936ceef0c8a0781cbe3cf394786d6d9dc4
Ritik Bhardwaj <varribhar@gmail.com>
2022-10-14 09:38:50 +0530

    adedd

--- Practice/itemsInEachPage.js
+++ Practice/itemsInEachPage.js
@@ -1,11 +1,11 @@
 class Splitter {
     constructor(items,itemsPerPage){
-        this.items
-        this.itemsPerPage
+        this.items = items
+        this.itemsPerPage = itemsPerPage
     }
 }
 let items = [1,2,"ritik","aakash",3]
-let itemsPerPage
+let itemsPerPage = 2
 const pageSplitter = new Splitter(items,itemsPerPage)
 
 console.log(pageSplitter)
